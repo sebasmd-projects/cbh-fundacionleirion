1f2cba5ffa67a33d0a22a736ebc2f90c59435570
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-08-23 02:31:58 -0500

    Mostrar el display en el quantity_type

--- apps/project/specific/assets/models.py
+++ apps/project/specific/assets/models.py
@@ -76,7 +76,7 @@ class AssetModel(TimeStampedModel):
     )
 
     def __str__(self) -> str:
-        return f"{self.name} - {self.quantity_type}"
+        return f"{self.name} - {self.get_quantity_type_display()}"
 
     class Meta:
         db_table = "apps_project_specific_assets_asset"
