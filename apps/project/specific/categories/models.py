f528bf4b1a668ebeaa61dc50707b9e57e4c40502
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-08-23 01:02:16 -0500

    unique_together en categorías

--- apps/project/specific/categories/models.py
+++ apps/project/specific/categories/models.py
@@ -4,11 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 from apps.common.utils.models import TimeStampedModel
 
+
 class AssetCategoryModel(TimeStampedModel):
     name = models.CharField(
         _("category"),
         max_length=50,
-        unique=True
     )
 
     description = models.TextField(
@@ -28,16 +28,17 @@ class AssetCategoryModel(TimeStampedModel):
 
     def __str__(self) -> str:
         message = f"{self.name}"
-        
+
         if self.parent:
             message = f"{self.parent} - {self.name}"
-            
+
         return f"{message}"
 
     class Meta:
         db_table = "apps_project_specific_categories_assetcategory"
         verbose_name = _('Asset Category')
         verbose_name_plural = _('Assets Categories')
+        unique_together = [['name', 'parent']]
 
 
 auditlog.register(
