5ef262a0f56e466282f45b938810d5d2ff8da774
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-08-23 01:16:04 -0500

    Custom Category Model Admin

--- apps/project/specific/categories/admin.py
+++ apps/project/specific/categories/admin.py
@@ -3,4 +3,12 @@ from import_export.admin import ImportExportActionModelAdmin
 
 from .models import AssetCategoryModel
 
-admin.site.register(AssetCategoryModel, ImportExportActionModelAdmin)
+
+@admin.register(AssetCategoryModel)
+class AssetCategoryModelAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
+    search_fields = ['name', 'parent__name']
+    list_filter = ['parent', 'created', 'is_active']
+    list_display = ['name', 'parent', 'created']
+    list_display_links = ['name']
+    ordering = ['default_order', 'name', 'parent', 'created']
+    readonly_fields = ['created', 'updated']
