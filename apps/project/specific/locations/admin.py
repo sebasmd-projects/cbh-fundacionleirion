10464d5dd4bf2d7dd398ae98d4d17ab771f1f85e
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-09-01 02:42:03 -0500

    AssetLocationModel admin visible por permiso

--- apps/project/specific/locations/admin.py
+++ apps/project/specific/locations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
+from django.conf import settings
 
 from .models import AssetLocationModel, LocationModel
 
@@ -8,6 +9,11 @@ from .models import AssetLocationModel, LocationModel
 class AssetLocationAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     search_fields = ['location__reference', 'location__owner', 'asset__name', 'asset__es_name']
     autocomplete_fields = ['asset']
+    
+    def has_module_permission(self, request):
+        if request.user.is_superuser or request.user.groups.filter(name=settings.EDIT_ASSETS_LOCATION).exists():
+            return True
+        return False
 
 
 @admin.register(LocationModel)
