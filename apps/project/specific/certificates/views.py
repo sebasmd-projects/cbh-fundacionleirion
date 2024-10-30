8f4d830802f916818af02e1092d77d98c762224b
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-10-30 16:13:24 -0500

    QR code and Bar code with uuid and translations

--- apps/project/specific/certificates/views.py
+++ apps/project/specific/certificates/views.py
@@ -1,14 +1,22 @@
+import base64
 from datetime import timedelta
+from io import BytesIO
 
+import barcode
+import qrcode
+import requests
+from barcode.writer import ImageWriter
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import (DetailView, FormView, ListView, TemplateView,
                                   View)
+from PIL import Image
 
 from .forms import IDNumberForm
 from .models import CertificateModel
@@ -36,7 +44,7 @@ class LockoutTimeView(View):
 class CertificateInputView(FormView):
     template_name = 'certificate_input.html'
     form_class = IDNumberForm
-    
+
     def form_valid(self, form):
         max_attempts = 5
         lockout_duration = timedelta(minutes=60)
@@ -57,7 +65,7 @@ class CertificateInputView(FormView):
 
         document_type = form.cleaned_data['document_type']
         document_number = form.cleaned_data['document_number']
-        
+
         try:
             certificate = CertificateModel.objects.get(
                 document_type=document_type,
@@ -87,6 +95,56 @@ class CertificateDetailView(DetailView):
     model = CertificateModel
     template_name = 'certificate_detail.html'
     context_object_name = 'certificate'
+
+    def generate_qr_with_favicon(self, url):
+        # Crear el código QR
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        img_qr = qr.make_image(fill="black", back_color="white").convert("RGB")
+
+        # Insertar favicon en el centro
+        icon_url = "https://cbh.fundacionleirion.com/static/imgs/favicon/android-chrome-512x512.png"
+        response = requests.get(icon_url)
+        icon = Image.open(BytesIO(response.content))
+        
+        icon = icon.resize((img_qr.size[0] // 4, img_qr.size[1] // 4), Image.LANCZOS)
+        pos = ((img_qr.size[0] - icon.size[0]) // 2, (img_qr.size[1] - icon.size[1]) // 2)
+        img_qr.paste(icon, pos, icon)
+
+        buffer = BytesIO()
+        img_qr.save(buffer, format="PNG")
+        qr_base64 = base64.b64encode(buffer.getvalue()).decode()
+        return f"data:image/png;base64,{qr_base64}"
+
+    def generate_barcode(self, uuid_str):
+        buffer = BytesIO()
+        barcode_class = barcode.get_barcode_class('code128')
+        barcode_image = barcode_class(uuid_str, writer=ImageWriter())
+        barcode_image.write(buffer)
+        barcode_base64 = base64.b64encode(buffer.getvalue()).decode()
+        return f"data:image/png;base64,{barcode_base64}"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        certificate_url = "https://cbh.fundacionleirion.com/certificate/{}".format(
+            self.object.pk
+        )
+
+        # Generar códigos bajo demanda
+        context['qr_code'] = mark_safe(
+            self.generate_qr_with_favicon(certificate_url)
+        )
+        context['barcode'] = mark_safe(
+            self.generate_barcode(str(self.object.pk))
+        )
+        return context
 
 
 class CertificateListView(PermissionRequiredMixin, ListView):
